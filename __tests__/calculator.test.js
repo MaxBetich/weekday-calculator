a63b674128c3fba35778d329eb2ac133b1a17de7
Max Betich <betichm@gmail.com>
2023-04-13 09:14:43 -0700

    update functionality for dates prior to 2001

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -19,7 +19,7 @@ describe('Date', () => {
     futureDate.convertDateToDays();
     pastDate.convertDateToDays();
     expect(futureDate.daysSince2001).toEqual(63);
-    expect(pastDate.daysSince2001).toEqual(6);
+    expect(pastDate.daysSince2001).toEqual(7);
   });
 
   test('should correctly return the day of the week for the given date in 2001', () => {
@@ -31,23 +31,68 @@ describe('Date', () => {
 
   test('should correctly return the number of days the date is away from January 1, 2001 for a date after 2001', () => {
     let futureDate = new Date(5,3,2002);
+    let futureDate2 = new Date(6,3,2002);
+    let futureDate3 = new Date(7,3,2002);
+    let futureDate4 = new Date(8,3,2002);
+    let futureDate5 = new Date(9,3,2002);
+    let futureDate6 = new Date(10,3,2002);
+    let futureDate7 = new Date(11,3,2002);
     let wayFutureDate = new Date(5,3,2012); 
     wayFutureDate.convertDateToDays();
     futureDate.convertDateToDays();
+    futureDate2.convertDateToDays();
+    futureDate3.convertDateToDays();
+    futureDate4.convertDateToDays();
+    futureDate5.convertDateToDays();
+    futureDate6.convertDateToDays();
+    futureDate7.convertDateToDays();
     wayFutureDate.findDayOfWeek();
+    futureDate.findDayOfWeek();
+    futureDate2.findDayOfWeek();
+    futureDate3.findDayOfWeek();
+    futureDate4.findDayOfWeek();
+    futureDate5.findDayOfWeek();
+    futureDate6.findDayOfWeek();
+    futureDate7.findDayOfWeek();
     expect(futureDate.daysSince2001).toEqual(428);
     expect(wayFutureDate.daysSince2001).toEqual(4081);
-    expect(wayFutureDate.dayOfTheWeek).toEqual("monday");
+    expect(futureDate.dayOfTheWeek).toEqual("tuesday");
+    expect(futureDate2.dayOfTheWeek).toEqual("wednesday");
+    expect(futureDate3.dayOfTheWeek).toEqual("thursday");
+    expect(futureDate4.dayOfTheWeek).toEqual("friday");
+    expect(futureDate5.dayOfTheWeek).toEqual("saturday");
+    expect(futureDate6.dayOfTheWeek).toEqual("sunday");
+    expect(futureDate7.dayOfTheWeek).toEqual("monday");
   });
 
   test('should correctly return the day of the week of a date before 2001', () => {
-    let pastDate = new Date(5,3,2000);
-    let wayPastDate = new Date(5,3,1996);
+    let pastDate = new Date(17,12,2000);
+    let wayPastDate = new Date(5,3,1999);
+    let wayPastDate2 = new Date(4,3,1999);
+    let wayPastDate3 = new Date(3,3,1999);
+    let wayPastDate4 = new Date(2,3,1999);
+    let wayPastDate5 = new Date(1,3,1999);
+    let wayPastDate6 = new Date(7,3,1998);
     pastDate.convertDateToDays();
     wayPastDate.convertDateToDays();
+    wayPastDate2.convertDateToDays();
+    wayPastDate3.convertDateToDays();
+    wayPastDate4.convertDateToDays();
+    wayPastDate5.convertDateToDays();
+    wayPastDate6.convertDateToDays();
     pastDate.findDayOfWeek();
     wayPastDate.findDayOfWeek();
+    wayPastDate2.findDayOfWeek();
+    wayPastDate3.findDayOfWeek();
+    wayPastDate4.findDayOfWeek();
+    wayPastDate5.findDayOfWeek();
+    wayPastDate6.findDayOfWeek();
     expect(pastDate.dayOfTheWeek).toEqual("sunday");
-    expect(wayPastDate.dayOfTheWeek).toEqual("tuesday");
+    expect(wayPastDate.dayOfTheWeek).toEqual("friday");
+    expect(wayPastDate2.dayOfTheWeek).toEqual("thursday");
+    expect(wayPastDate3.dayOfTheWeek).toEqual("wednesday");
+    expect(wayPastDate4.dayOfTheWeek).toEqual("tuesday");
+    expect(wayPastDate5.dayOfTheWeek).toEqual("monday");
+    expect(wayPastDate6.dayOfTheWeek).toEqual("saturday");
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -41,7 +41,8 @@ export class Date {
       }
       this.daysSince2001+=this.day;
       this.daysSince2001 -= 1;
-  } else if (this.year < 2001)  {
+  } else {
+    //For dates before 2001
     for (let year = 2000; year > this.year; year --){
       if (checkLeapYear(year) === false) {
         this.daysSince2001 += 365;
@@ -51,36 +52,53 @@ export class Date {
     }
     if (checkLeapYear(this.year) === true) {
       daysInMonthsArray = [31,29,31,30,31,30,31,31,30,31,30,31];
+      
     } else {
       daysInMonthsArray = [31,28,31,30,31,30,31,31,30,31,30,31];
     }
     for (let i = 11; i > this.month-1; i--) {
-      this.daysSince2001 += daysInMonthsArray[i+1];
-      //console.log(this.daysSince2001);
+      this.daysSince2001 += daysInMonthsArray[i];
+      
     }
-    this.daysSince2001 = (this.daysSince2001 + (daysInMonthsArray[this.month-1])-this.day);
+    this.daysSince2001 = (this.daysSince2001 + (daysInMonthsArray[this.month-1])-this.day + 1);
   }
 } 
 
   findDayOfWeek() {
     const daysRemainder = this.daysSince2001%7;
-    console.log(daysRemainder);
-    if (daysRemainder === 0) {
-      this.dayOfTheWeek = "monday";
-    } else if (daysRemainder === 1) {
-      this.dayOfTheWeek = "tuesday";
-    } else if (daysRemainder === 2) {
-      this.dayOfTheWeek = "wednesday";
-    } else if (daysRemainder === 3) {
-      this.dayOfTheWeek = "thursday";
-    } else if (daysRemainder === 4) {
-      this.dayOfTheWeek = "friday";
-    } else if (daysRemainder === 5) {
-      this.dayOfTheWeek = "saturday";
-    } else if (daysRemainder === 6) {
-      this.dayOfTheWeek = "sunday";
-    } 
-    //console.log(this);
+    if (this.year >= 2001) {
+      if (daysRemainder === 0) {
+        this.dayOfTheWeek = "monday";
+      } else if (daysRemainder === 1) {
+        this.dayOfTheWeek = "tuesday";
+      } else if (daysRemainder === 2) {
+        this.dayOfTheWeek = "wednesday";
+      } else if (daysRemainder === 3) {
+        this.dayOfTheWeek = "thursday";
+      } else if (daysRemainder === 4) {
+        this.dayOfTheWeek = "friday";
+      } else if (daysRemainder === 5) {
+        this.dayOfTheWeek = "saturday";
+      } else {
+        this.dayOfTheWeek = "sunday";
+      } 
+    } else {
+      if (daysRemainder === 0) {
+        this.dayOfTheWeek = "monday";
+      } else if (daysRemainder === 1) {
+        this.dayOfTheWeek = "sunday";
+      } else if (daysRemainder === 2) {
+        this.dayOfTheWeek = "saturday";
+      } else if (daysRemainder === 3) {
+        this.dayOfTheWeek = "friday";
+      } else if (daysRemainder === 4) {
+        this.dayOfTheWeek = "thursday";
+      } else if (daysRemainder === 5) {
+        this.dayOfTheWeek = "wednesday";
+      } else {
+        this.dayOfTheWeek = "tuesday";
+      }
+    }
   }
 
 
